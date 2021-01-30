57afd73e39f0fb0942196dd8fbb17ca769208bb6
kabaakk <fb1907rsl@hotmail.com>
2021-01-30 15:17:50 +0300

    Add randomGeneration fruitFalling and collectible objects

---
+++ Assets/Script/FruitFalling.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitFalling : MonoBehaviour
+{
+    public float speed = 10f;
+    public Transform player;
+    float rand;
+    public float minX, minY;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
+
+        if (transform.position.y < player.position.y) 
+        {
+            rand = Random.Range(minX, minY);
+            transform.position = new Vector3(rand, transform.position.y, transform.position.z);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            //health decrease;
+            Debug.Log("Fruit crash player");
+            rand = Random.Range(minX, minY);
+            transform.position = new Vector3(rand, transform.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Script/RandomSprite.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSprite : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowRandomSprite();
+    }
+
+    private void ShowRandomSprite()
+    {
+        int index = UnityEngine.Random.Range(0, 3);
+        int childCount = transform.childCount;
+
+        for (int i = 0; i < childCount; i++)
+        {
+            Transform child = transform.GetChild(i);
+            bool shouldShow = index == i;
+
+            child.gameObject.SetActive(shouldShow);
+        }
+    }
+
+    private void OnEnable()
+    {
+        ShowRandomSprite();
+    }
+}
